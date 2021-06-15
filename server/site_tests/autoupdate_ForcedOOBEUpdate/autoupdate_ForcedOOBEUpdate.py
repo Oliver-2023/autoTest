msft-mirror-aosp/platform.external.autotest

Update all AU tests to use FakeOmaha.
This CL does two things:
1) fixes the remaining OOBE test failure which requires a different
value for return_noupdate_starting.
2) Generalizes get_update_url_for_test so that all tests that need to
use FakeOmaha will do so.

BUG=chromium:1189379
TEST=OOBE, Interrupt, Rollback test and fails at launching fakeomaha.

Change-Id: Id10483d5bfb801fdcec5fd048dc576ee3de328bf
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2837187
Tested-by: David Haddock <dhaddock@chromium.org>
Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>
Commit-Queue: David Haddock <dhaddock@chromium.org>

server/site_tests/autoupdate_ForcedOOBEUpdate/autoupdate_ForcedOOBEUpdate.py
@@ -109,7 +109,8 @@ class autoupdate_ForcedOOBEUpdate(update_engine_test.UpdateEngineTest):
                  interrupt=None,
                  job_repo_url=None,
                  moblab=False,
-                 m2n=False):
+                 m2n=False,
+                 return_noupdate_starting=2):
         """
         Runs a forced autoupdate during ChromeOS OOBE.
 
@@ -124,6 +125,11 @@ class autoupdate_ForcedOOBEUpdate(update_engine_test.UpdateEngineTest):
         @param moblab: True if we are running on moblab.
         @param m2n: True if we should first provision the latest stable version
                     for the current board so that we can perform a M->N update.
+        @param return_noupdate_starting: (Number of times - 1) we want
+                                         FakeOmaha to return an update before
+                                         returning noupdate. 1 means never
+                                         return noupdate. 2 means return
+                                         noupdate after 1 update response.
 
         """
         if interrupt and interrupt not in self._SUPPORTED_INTERRUPTS:
@@ -154,14 +160,11 @@ class autoupdate_ForcedOOBEUpdate(update_engine_test.UpdateEngineTest):
             payload_url = self.get_payload_url_on_public_bucket(
                 job_repo_url, full_payload=full_payload)
         else:
-            if moblab:
-                update_url = self.get_update_url_for_test(
-                        job_repo_url, full_payload=full_payload)
-            else:
-                update_url = self.get_update_url_from_fake_omaha(
-                        job_repo_url,
-                        full_payload=full_payload,
-                        return_noupdate_starting=2)
+            update_url = self.get_update_url_for_test(
+                    job_repo_url,
+                    full_payload=full_payload,
+                    moblab=moblab,
+                    return_noupdate_starting=return_noupdate_starting)
         before_version = self._host.get_release_version()
 
         # Clear any previously started updates.

Oliver-2023/autoTest

(cherry picked from commit f22a3fd2b15037691182c1b724b5ccaa9f0e6079)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/autotest/+/2961492
Reviewed-by: Derek Beckett <dbeckett@chromium.org>
Reviewed-by: Congbin Guo <guocb@chromium.org>
Commit-Queue: Congbin Guo <guocb@chromium.org>

server/site_tests/autoupdate_ForcedOOBEUpdate/autoupdate_ForcedOOBEUpdate.py
@@ -101,8 +101,13 @@ class autoupdate_ForcedOOBEUpdate(update_engine_test.UpdateEngineTest):
                                  desc='post-reboot event to fire after reboot')
 
 
-    def run_once(self, full_payload=True, cellular=False,
-                 interrupt=None, job_repo_url=None, moblab=False):
+    def run_once(self,
+                 full_payload=True,
+                 cellular=False,
+                 interrupt=None,
+                 job_repo_url=None,
+                 moblab=False,
+                 return_noupdate_starting=2):
         """
         Runs a forced autoupdate during ChromeOS OOBE.
 
@@ -115,6 +120,11 @@ class autoupdate_ForcedOOBEUpdate(update_engine_test.UpdateEngineTest):
                              The test will read this from a host argument
                              when run in the lab.
         @param moblab: True if we are running on moblab.
+        @param return_noupdate_starting: (Number of times - 1) we want
+                                         FakeOmaha to return an update before
+                                         returning noupdate. 1 means never
+                                         return noupdate. 2 means return
+                                         noupdate after 1 update response.
 
         """
         if interrupt and interrupt not in self._SUPPORTED_INTERRUPTS:
@@ -131,14 +141,11 @@ class autoupdate_ForcedOOBEUpdate(update_engine_test.UpdateEngineTest):
             payload_url = self.get_payload_url_on_public_bucket(
                 job_repo_url, full_payload=full_payload)
         else:
-            if moblab:
-                update_url = self.get_update_url_for_test(
-                        job_repo_url, full_payload=full_payload)
-            else:
-                update_url = self.get_update_url_from_fake_omaha(
-                        job_repo_url,
-                        full_payload=full_payload,
-                        return_noupdate_starting=2)
+            update_url = self.get_update_url_for_test(
+                    job_repo_url,
+                    full_payload=full_payload,
+                    moblab=moblab,
+                    return_noupdate_starting=return_noupdate_starting)
         before_version = self._host.get_release_version()
 
         # Clear any previously started updates.